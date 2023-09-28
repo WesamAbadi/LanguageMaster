d49b9049ba7e504bb2ebee50ca4a9b4967ee7c42
Wesam Abadi <wisamabady7@gmail.com>
2023-09-28 12:55:36 +0200

    switch to scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { auth, googleProvider } from "./config/firebase-config";
