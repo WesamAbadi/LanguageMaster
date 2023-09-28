93c2b67a4fd137b36bbd205c2213fa1742629104
Wesam Abadi <wisamabady7@gmail.com>
2023-09-28 16:58:33 +0200

    add tabswitcher to the admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,46 +1,50 @@
 import React, { useState } from "react";
 import AddLanguage from "../components/AddLanguage";
 import AddLesson from "../components/AddLesson/AddLesson";
+import TabSwitch from "../components/TabSwitch";
 import "../styles/pages/Admin.css";
 
 function Admin() {
   const [activeTab, setActiveTab] = useState(0);
+  const tabNames = [
+    "Add a new language",
+    "Add a new lesson",
+    "Manage feedbacks",
+    "Manage users",
+  ];
+
   const [feedback, setFeedback] = useState({ message: "", type: "" });
 
   const updateFeedback = (message, type) => {
     setFeedback({ message, type });
   };
 
-  const renderTabContent = () => {
-    switch (activeTab) {
-      case 1:
-        return <AddLanguage updateFeedback={updateFeedback} />;
-      case 2:
-        return <AddLesson updateFeedback={updateFeedback} />;
-      case 3:
-        return null;
-      case 4:
-        return null;
-      default:
-        return (
-          <div>
-            <p>This is your dashboard, Admin!</p>
-            <p>Start by selecting a tab</p>
-          </div>
-        );
-    }
+  const handleTabChange = (direction) => {
+    setActiveTab(direction);
+    console.log(`Active tab is now: ${activeTab}`);
   };
 
   return (
     <div>
       <h2>Admin Page</h2>
       <div>
-        <div className="tab-navigation">
-          <button onClick={() => setActiveTab(1)}>Add a new language</button>
-          <button onClick={() => setActiveTab(2)}>Add a new lesson</button>
-          <button onClick={() => setActiveTab(3)}>Manage feedbacks</button>
-          <button onClick={() => setActiveTab(4)}>Manage users</button>
-        </div>
+        <TabSwitch
+          activeTab={activeTab}
+          onTabChange={handleTabChange}
+          tabs={tabNames}
+        />
+
+        {activeTab === 0 ? (
+          <AddLanguage updateFeedback={updateFeedback} />
+        ) : activeTab === 1 ? (
+          <AddLesson updateFeedback={updateFeedback} />
+        ) : activeTab === 2 ? (
+          <p>SOON</p>
+        ) : activeTab === 3 ? (
+          <p>SOON</p>
+        ) : (
+          <p>Please select a tab</p>
+        )}
         <div>
           {feedback.message && (
             <div
@@ -57,7 +61,6 @@ function Admin() {
             </div>
           )}
         </div>
-        <div className="tab-content">{renderTabContent()}</div>
       </div>
     </div>
   );
