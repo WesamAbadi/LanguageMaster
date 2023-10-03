212d0b1b47a4ccd82aa0ef098927e8f2c5303c91
Wesam Abadi <wisamabady7@gmail.com>
2023-10-03 15:01:08 +0200

    better auth method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -17,26 +17,42 @@ import Landing from "./pages/Landing";
 import Soon from "./pages/Soon";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState(null);
 
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, googleProvider).then((result) => {
-      localStorage.setItem("isAuth", true);
-      setIsAuth(true);
+  useEffect(() => {
+    // Set up an authentication observer
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // User is signed in.
+        setUser(authUser);
+      } else {
+        // User is signed out.
+        setUser(null);
+      }
     });
+
+    // Clean up the observer when the component unmounts
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
   };
 
-  const signOutUser = () => {
-    signOut(auth)
-      .then(() => {
-        localStorage.setItem("isAuth", false);
-        setIsAuth(false);
-        console.log("User signed out");
-      })
-      .catch((error) => {
-        console.error("Error signing out:", error);
-      });
+  const signOutUser = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
   };
+
   return (
     <Router>
       <nav>
@@ -44,8 +60,11 @@ function App() {
           <NavLink to="/Home">Home</NavLink>
           <NavLink to="/Admin">Admin</NavLink>
         </div>
-        {isAuth ? (
-          <button onClick={signOutUser}>Sign out</button>
+        {user ? (
+          <div>
+            <p>Welcome, {user.displayName}!</p>
+            <button onClick={signOutUser}>Sign out</button>
+          </div>
         ) : (
           <button onClick={signInWithGoogle}>Sign in with Google</button>
         )}
