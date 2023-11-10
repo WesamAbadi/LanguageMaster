3f0d4c9922bfa01674d2aa5f8ad5d1716b9eddfc
Wesam Abadi <wisamabady7@gmail.com>
2023-11-10 21:40:43 +0100

    add loading screen

--- src/App.js
+++ src/App.js
@@ -113,7 +113,34 @@ function App() {
   };
 
   if (loading) {
-    return <div style={{ color: "white" }}>Loading...</div>;
+    return (
+      <div class="loading-screen">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <defs>
+            <filter id="gooey">
+              <feGaussianBlur
+                in="SourceGraphic"
+                stdDeviation="10"
+                result="blur"
+              ></feGaussianBlur>
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+                result="goo"
+              ></feColorMatrix>
+              <feBlend in="SourceGraphic" in2="goo"></feBlend>
+            </filter>
+          </defs>
+        </svg>
+        <div class="blob blob-0"></div>
+        <div class="blob blob-1"></div>
+        <div class="blob blob-2"></div>
+        <div class="blob blob-3"></div>
+        <div class="blob blob-4"></div>
+        <div class="blob blob-5"></div>
+      </div>
+    );
   }
   return (
     <div>
