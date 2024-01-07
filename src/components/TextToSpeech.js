65489c15548bcd030a08c3f5b474f777c886b438
Wesam Abadi <wisamabady7@gmail.com>
2024-01-07 14:13:33 +0100

    add backup api

--- src/components/TextToSpeech.js
+++ src/components/TextToSpeech.js
@@ -6,13 +6,17 @@ import "../styles/components/TextToSpeech.scss";
 function TextToSpeech({ index, text, languageCode }) {
   const [loading, setLoading] = useState(false);
   const [showDiv, setShowDiv] = useState(false);
+  const primaryApiUrl = "https://text-to-speech-api3.p.rapidapi.com";
+  const fallbackApiUrl =
+    "https://realistic-text-to-speech.p.rapidapi.com/v3/generate_voice_over_v2";
 
   const apiKey = process.env.REACT_APP_RAPIDAPI_KEY;
   const call = async () => {
     setLoading(true);
+
     const options = {
       method: "GET",
-      url: "https://text-to-speech-api3.p.rapidapi.com/speak",
+      url: primaryApiUrl + "/speak",
       params: {
         text: text,
         lang: languageCode || "en",
@@ -20,7 +24,7 @@ function TextToSpeech({ index, text, languageCode }) {
       responseType: "arraybuffer",
       headers: {
         "X-RapidAPI-Key": apiKey,
-        "X-RapidAPI-Host": "text-to-speech-api3.p.rapidapi.com",
+        "X-RapidAPI-Host": primaryApiUrl,
       },
     };
 
@@ -32,9 +36,60 @@ function TextToSpeech({ index, text, languageCode }) {
       audio.play();
     } catch (error) {
       console.error(error);
-    }
+      const encodedParams = new URLSearchParams();
+      encodedParams.set("src", "Hello, world!");
+      encodedParams.set("hl", "en-us");
+      encodedParams.set("r", "0");
+      encodedParams.set("c", "mp3");
+      encodedParams.set("f", "8khz_8bit_mono");
 
-    setLoading(false);
+      const optionsFallback = {
+        method: "POST",
+        url: fallbackApiUrl,
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key": apiKey,
+          "X-RapidAPI-Host": "realistic-text-to-speech.p.rapidapi.com",
+        },
+        data: {
+          voice_obj: {
+            id: 2014,
+            voice_id: "en-US-Neural2-A",
+            gender: "Male",
+            language_code: "en-US",
+            language_name: "US English",
+            voice_name: "John",
+            sample_text:
+              "Hello, hope you are having a great time making your video.",
+            sample_audio_url:
+              "https://s3.ap-south-1.amazonaws.com/invideo-uploads-ap-south-1/speechen-US-Neural2-A16831901130600.mp3",
+            status: 2,
+            rank: 0,
+            type: "google_tts",
+            isPlaying: false,
+          },
+          json_data: [
+            {
+              block_index: 0,
+              text: text,
+            },
+          ],
+        },
+      };
+
+      try {
+        console.log("converting failed, attempting another fallback API (english only)...");
+        const fallbackResponse = await axios.request(optionsFallback);
+        console.log(fallbackResponse.data);
+        const audioURL = fallbackResponse.data[0].link;
+        const audio = new Audio(audioURL);
+        audio.play();
+      } catch (fallbackError) {
+        console.error(fallbackError);
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
